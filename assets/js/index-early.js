1b7ef54e291dfa3cbce5b4acbeca4c8fd7c60fb2
bichchixanh-collab <bichchixanh@gmail.com>
2026-09-26 07:41:28 +0700

    revert: inline lai game/index JS+CSS (goi tach module loi), CSP unsafe-inline, go timeout

--- assets/js/index-early.js
+++
@@ -1,3 +0,0 @@
-if('serviceWorker' in navigator){window.addEventListener('load',function(){navigator.serviceWorker.register('sw.js').catch(function(){});});}
-
-try{if(localStorage.getItem('j2me_lite')==='1')document.documentElement.classList.add('lite');}catch(e){}
--- assets/js/lite-init.js
+++
@@ -1,1 +0,0 @@
-try{if(localStorage.getItem('j2me_lite')==='1')document.documentElement.classList.add('lite');}catch(e){}
